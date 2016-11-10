fe9274e3fc89448ad9601fd488edba47870f74e4
VZ <coder.vz@gmail.com>
2016-11-10 09:32:10 +0700

    Задача 2A: A + B

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express'
+
+const app = express()
+
+app.get('/', function (req, res) {
+  let a = parseInt( req.query.a );
+  let b = parseInt( req.query.b );
+  res.send(`Вывод: ${ (isNaN(a) ? 0 : a) + (isNaN(b) ? 0 : b)}` )
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
