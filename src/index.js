afad22964254f8a8cdc43aea4585540a1c644949
VZ <coder.vz@gmail.com>
2016-11-16 23:24:19 +0700

    fixed Wed Nov 16 23:24:19 +07 2016

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import cors from 'cors';
-import fetch from 'isomorphic-fetch';
+import fetch from 'isomorphic-fetch'; 
 import _ from 'lodash';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
@@ -17,101 +17,45 @@ fetch(pcUrl)
     console.log('Чтото пошло не так:', err);
   });
 
-function testNativeProperty(obj, property) {
-  if (!obj.hasOwnProperty(property)) {
-    throw new Error('Property true');
-  } else 
-  if (obj.constructor()[property] !== undefined) {
-    throw new Error('Property true');
-  }
-}
 
-app.get('/task3A', async (req, res) => {
-    res.json(pc);
-});
-
-app.get('/task3A/:devace', async (req, res) => {
+app.get('/task3A*', (req, res) => {
   try {
-    let devace = req.params.devace;
-    let itog = {};
-    if (devace === 'volumes') { 
+    let path = [];
+    req.url.replace('/task3A','').split('/').map((value) => {
+      if (value != '') path.push(value);
+    });
+
+    if(path.length === 0) {
+      res.json(pc);
+    } else if(path[0] === 'volumes') { 
       let volume = {};
+      let arrVolumes = {};
       pc['hdd'].map((value, index)=> {
           volume = _.pick(value, ['size','volume']);
-          itog[volume.volume] = volume.size + (itog[volume.volume] ? +itog[volume.volume].replace('B','') : 0) + 'B'; 
+          arrVolumes[volume.volume] = volume.size + (arrVolumes[volume.volume] ? +arrVolumes[volume.volume].replace('B','') : 0) + 'B'; 
       });
-    } else {
-      testNativeProperty(pc,devace);
-      itog = pc[devace];
-    }
 
-    if(itog === undefined ) {
-      throw new Error('Not found');
-    } else {
-      res.json(itog);
-    }
-  } catch (err) {
-    res.sendStatus(404);
-  }
-});
+      res.json(arrVolumes);
+    } else { 
+      let response = pc;
+      path.map((prop) => {
+        if(response.constructor()[prop] !== undefined) {
+            throw new Error('No data');
+        } 
+        
+        response = response[prop];
+      });
 
-app.get('/task3A/:devace/:params', async (req, res) => {  
-
-  try {
-    let devace = req.params.devace;
-    let params = req.params.params;
-
-    testNativeProperty(pc,devace);
-    testNativeProperty(pc[devace],params);
-
-    let param = pc[devace][params];
-    if (param) {
-      res.json(param);
-    } else {
-      throw new Error('Not found');
+      if (response !== undefined) {
+        res.json(response);
+      } else {
+        throw new Error('No data');
+      } 
     }
   } catch (err) {
     res.sendStatus(404);
   }
 });
-
-app.get('/task3A/:devace/:params/:end*', async (req, res) => {
-  try {
-    if (req.params[0]) throw new Error('Ложный параметр');
-    let devace = req.params.devace;
-    let params = req.params.params;
-    let end = req.params.end;
-    testNativeProperty(pc,devace);
-    testNativeProperty(pc[devace],params);
-    testNativeProperty(pc[devace][params],end);
-    let data;
-    if (devace === 'hdd') {
-      let volume;
-      if (pc[devace][params][end]) { 
-        pc['hdd'].map((value, index)=> {
-          if (index == req.params.params) {
-            if (end != 'size') {
-              res.send(`"${_.pick(value, [end])[end]}"`);
-            } else {
-              res.json(_.pick(value, [end])[end]);
-            }
-          } 
-        });
-      } else throw new Error('Not found');
-    } else {
-      if (pc[devace][params][end]) {
-        res.json(pc[devace][params][end])
-      } else throw new Error('Not found');
-    }
-  } catch (err) {
-    res.sendStatus(404);
-  }
-});
-
-app.use(function (req, res) {
-  res.sendStatus(404);
-});
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
