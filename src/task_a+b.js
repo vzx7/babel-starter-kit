a3f2c882995ab2f232bad6d4c041075414bd5761
VZ <coder.vz@gmail.com>
2016-11-17 06:38:07 +0700

    refactoring

--- src/task_a+b.js
+++ src/task_a+b.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import cors from 'cors';
 
-const app = express()
+const app = express();
+app.use(cors());
 
 app.get('/', function (req, res) {
   let a = parseInt( req.query.a );
   let b = parseInt( req.query.b );
-  res.send(`Вывод: ${ (isNaN(a) ? 0 : a) + (isNaN(b) ? 0 : b)}` )
+  res.send(`${ (isNaN(a) ? 0 : a) + (isNaN(b) ? 0 : b)}` )
 })
 
 app.listen(3000, function () {
